32126461cb318d5a214dbc77bb34e724b903fba4
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-03-30 10:58:01 +0700

    add sql query & controller

--- src/java_crud_mvc/Controller/MemberController.java
+++ src/java_crud_mvc/Controller/MemberController.java
@@ -4,10 +4,116 @@
  */
 package java_crud_mvc.Controller;
 
+import java.util.List;
+import java_crud_mvc.DAO.MemberDAO;
+import java_crud_mvc.DAOImplement.MemberImpl;
+import java_crud_mvc.Model.MemberModel;
+import java_crud_mvc.Model.MemberModelTable;
+import java_crud_mvc.View.MemberView;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Lenovo
  */
 public class MemberController {
+    MemberView memberFrame;
+    MemberImpl memberImpl;
     
+    List<MemberModel> listMember;
+    
+    public MemberController(MemberView memberFrame) {
+        this.memberFrame = memberFrame;
+        memberImpl = new MemberDAO();
+        listMember = memberImpl.getAll();
+    }
+    
+    // Reset Button
+    public void resetTextField() {
+        memberFrame.getTxtID().setText("");
+        memberFrame.getTxtMemberName().setText("");
+        memberFrame.getTxtPhoneNumber().setText("");
+        memberFrame.getTxtAddress().setText("");
+        memberFrame.getCbMemberType().setSelectedItem("--- Choose Type ---");
+    }
+    
+    // Fill Data Table
+    public void fillTable() {
+        listMember = memberImpl.getAll();
+        MemberModelTable table = new MemberModelTable(listMember);
+        memberFrame.getTblData().setModel(table);
+    }
+    
+    // Show Data to Form while Clicked
+    public void fillField(int row) {
+        memberFrame.getTxtID().setText(listMember.get(row).getId().toString());
+        memberFrame.getTxtMemberName().setText(listMember.get(row).getName());
+        memberFrame.getTxtPhoneNumber().setText(listMember.get(row).getPhoneNumber());
+        memberFrame.getTxtAddress().setText(listMember.get(row).getAddress());
+        memberFrame.getCbMemberType().setSelectedItem(listMember.get(row).getMemberType());
+    }
+    
+    // Insert Data Form to Database
+    public void insert() {
+        if (!memberFrame.getTxtMemberName().getText().trim().isEmpty() &&
+                !memberFrame.getTxtPhoneNumber().getText().trim().isEmpty() &&
+                !memberFrame.getTxtAddress().getText().trim().isEmpty() &&
+                !memberFrame.getCbMemberType().getSelectedItem().equals("--- Choose Type ---")) {
+            MemberModel member = new MemberModel();
+            member.setName(memberFrame.getTxtMemberName().getText());
+            member.setPhoneNumber(memberFrame.getTxtPhoneNumber().getText());
+            member.setAddress(memberFrame.getTxtAddress().getText());
+            member.setMemberType(memberFrame.getCbMemberType().getSelectedItem().toString());
+            
+            memberImpl.insert(member);
+            JOptionPane.showMessageDialog(null, "Success Save Data!", "Success", 0);
+        } else {
+            JOptionPane.showMessageDialog(memberFrame, "Data cannot be null!", "Error", 0);
+        }
+    }
+    
+    // Update Data Form to Database 
+    public void update() {
+        if (!memberFrame.getTxtID().getText().trim().isEmpty()) {
+            MemberModel member = new MemberModel();
+            member.setName(memberFrame.getTxtMemberName().getText());
+            member.setPhoneNumber(memberFrame.getTxtPhoneNumber().getText());
+            member.setAddress(memberFrame.getTxtAddress().getText());
+            member.setMemberType(memberFrame.getCbMemberType().getSelectedItem().toString());
+            member.setId(Integer.parseInt(memberFrame.getTxtID().getText()));
+            
+            memberImpl.update(member);
+            JOptionPane.showMessageDialog(null, "Success Update Data!", "Success", 0);
+        } else {
+            JOptionPane.showMessageDialog(memberFrame, "Data cannot be null!", "Error", 0);
+        }
+    }
+    
+    // Delete Data Table
+    public void delete() {
+        if (!memberFrame.getTxtID().getText().trim().isEmpty()) {
+            int id = Integer.parseInt(memberFrame.getTxtID().getText());
+            memberImpl.delete(id);
+            JOptionPane.showMessageDialog(null, "Success Delete Data!", "Success", 0);
+        } else {
+            JOptionPane.showMessageDialog(memberFrame, "Choose data!", "Error", 0);
+        }
+    }
+    
+    // Search Data
+    public void fillSearchTable() {
+        listMember = memberImpl.getMember(memberFrame.getTxtSearch().getText());
+        MemberModelTable table = new MemberModelTable(listMember);
+        memberFrame.getTblData().setModel(table);
+    }
+    
+    // Get Search Data
+    public void getData() {
+        if (!memberFrame.getTxtSearch().getText().trim().isEmpty()) {
+            memberImpl.getMember(memberFrame.getTxtSearch().getText());
+            fillSearchTable();
+        } else {
+            JOptionPane.showMessageDialog(memberFrame, "Choose data!", "Error", 0);
+        }
+    }
 }
--- src/java_crud_mvc/DAO/MemberDAO.java
+++ src/java_crud_mvc/DAO/MemberDAO.java
@@ -4,10 +4,141 @@
  */
 package java_crud_mvc.DAO;
 
+import java.util.List;
+import java_crud_mvc.DAOImplement.MemberImpl;
+import java_crud_mvc.Model.MemberModel;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java_crud_mvc.Connection.DatabaseConnection;
+
 /**
  *
  * @author Lenovo
  */
-public class MemberDAO {
+public class MemberDAO implements MemberImpl{
+    
+    Connection connection;
+    
+    final String INSERT = "INSERT INTO member (name, phonenumber, address, membertype) VALUES(?, ?, ?, ?);";
+    final String UPDATE = "UPDATE member SET name=?, phonenumber=?, address=?, membertype=? WHERE id=?;";
+    final String DELETE = "DELETE FROM member WHERE id=?;";
+    final String SELECT = "SELECT * FROM member;";
+    final String SEARCH = "SELECT * FROM member WHERE name LIKE ?;";
+
+    public MemberDAO() {
+        connection = (Connection) DatabaseConnection.connection();
+    }
+
+    @Override
+    public void insert(MemberModel member) {
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(INSERT);
+            statement.setString(1, member.getName());
+            statement.setString(2, member.getPhoneNumber());
+            statement.setString(3, member.getAddress());
+            statement.setString(4, member.getMemberType());
+            statement.executeUpdate();
+            ResultSet result = statement.getGeneratedKeys();
+            while (result.next()) {
+                member.setId(result.getInt(1));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                statement.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void update(MemberModel member) {
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(UPDATE);
+            statement.setString(1, member.getName());
+            statement.setString(2, member.getPhoneNumber());
+            statement.setString(3, member.getAddress());
+            statement.setString(4, member.getMemberType());
+            statement.setInt(5, member.getId());
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                statement.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(DELETE);
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                statement.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public List<MemberModel> getAll() {
+        List<MemberModel> listMember = null;
+        try {
+            listMember = new ArrayList<MemberModel>();
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(SELECT);
+            while (result.next()) {
+                MemberModel member = new MemberModel();
+                member.setId(result.getInt("id"));
+                member.setName(result.getString("name"));
+                member.setPhoneNumber(result.getString("phonenumber"));
+                member.setAddress(result.getString("address"));
+                member.setMemberType(result.getString("membertype"));
+                listMember.add(member);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MemberDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listMember;
+    }
+
+    @Override
+    public List<MemberModel> getMember(String name) {
+        List<MemberModel> listMember = null;
+        try {
+            listMember = new ArrayList<MemberModel>();
+            PreparedStatement statement = connection.prepareStatement(SEARCH);
+            statement.setString(1, "%" + name + "%");
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                MemberModel member = new MemberModel();
+                member.setId(result.getInt("id"));
+                member.setName(result.getString("name"));
+                member.setPhoneNumber(result.getString("phonenumber"));
+                member.setAddress(result.getString("address"));
+                member.setMemberType(result.getString("membertype"));
+                listMember.add(member);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MemberDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listMember;
+    }
     
 }
